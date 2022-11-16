df03b7605deb140731d6267d8f241116d42ecfd1
Varga Mark <mrk_varga@yahoo.com>
2022-11-16 21:02:31 +0200

    Create user menu

--- src/main/java/com/sda/mark/bookmanagement/Main.java
+++ src/main/java/com/sda/mark/bookmanagement/Main.java
@@ -1,11 +1,43 @@
 package com.sda.mark.bookmanagement;
 
+import com.sda.mark.bookmanagement.menu.UserOption;
 import com.sda.mark.bookmanagement.utils.SessionManager;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         SessionManager.getSessionFactory();
+        Scanner scanner = new Scanner(System.in);
+
+        UserOption userOption;
+
+        do {
+            UserOption.printAllOptions();
+            System.out.println("Please select an option");
+            try {
+                int numericOption = Integer.parseInt(scanner.nextLine());
+                userOption = UserOption.findUserOption(numericOption);
+            } catch (NumberFormatException e) {
+                userOption = UserOption.UNKNOWN;
+            }
+
+            switch(userOption){
+                case CREATE_AUTHOR:
+                    System.out.println("Not implemented");
+                    break;
+                case EXIT:
+                    System.out.println("Goodbye!");
+                    break;
+                case UNKNOWN:
+                    System.out.println("Option Unknown");
+                    break;
+                default:
+                    System.out.println("User option " + userOption + "is not implemented!");
+            }
+        } while (userOption != UserOption.EXIT);
+        SessionManager.shutDown();
 
     }
 }
---
+++ src/main/java/com/sda/mark/bookmanagement/menu/UserOption.java
@@ -0,0 +1,40 @@
+package com.sda.mark.bookmanagement.menu;
+
+public enum UserOption {
+    CREATE_AUTHOR(1, "Create author"),
+    EXIT(99, "Exit"),
+    UNKNOWN(100, "Unknown option");
+
+    private int numericOption;
+    private String displayValue;
+
+    UserOption(int numericOption, String displayValue) {
+        this.numericOption = numericOption;
+        this.displayValue = displayValue;
+    }
+
+    public int getNumericOption() {
+        return numericOption;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
+
+    public static void printAllOptions(){
+        for (UserOption value : values()) {
+            if(value!= UNKNOWN){
+                System.out.println(value.getNumericOption() + " - " + value.getDisplayValue());
+            }
+        }
+    }
+
+    public static UserOption findUserOption(int numericValue){
+        for(UserOption value : values()){
+            if(value.getNumericOption() == numericValue){
+                return value;
+            }
+        }
+        return UNKNOWN;
+    }
+}
